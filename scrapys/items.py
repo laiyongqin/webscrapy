bf8cd4d012d324e0a2d23251489e8238ad4ae9fa
Owen <owen.zou@outlook.com>
2015-03-05 16:39:51 +0800

    update

--- scrapys/items.py
+++ scrapys/items.py
@@ -7,8 +7,27 @@
 
 import scrapy
 
-
 class ScrapysItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+class ProdItem(scrapy.Item):
+    category = scrapy.Field()
+    attrs = scrapy.Field()
+    skus = scrapy.Field()
+
+class TmallBrand(scrapy.Item):
+    # 主营业务
+    category = scrapy.Field()
+    brandName = scrapy.Field()
+    shopName = scrapy.Field()
+    wangwangUrl = scrapy.Field()
+    companyName = scrapy.Field()
+    shopType = scrapy.Field()
+    shopLocation = scrapy.Field()
+    tradeCount = scrapy.Field()
+    # 营业执照链接
+    ziZhaoUrl = scrapy.Field()
+    
+    
--- scrapys/spiders/tmall_brands.py
+++ scrapys/spiders/tmall_brands.py
@@ -5,6 +5,7 @@ import scrapy
 import json
 import urllib2
 import io
+from scrapys.items import TmallBrand
 
 class TmallBrandsSpider(scrapy.Spider):
     name = 'tmallbrands'
@@ -14,10 +15,11 @@ class TmallBrandsSpider(scrapy.Spider):
                  'callback': '1'
                  }
     f = io.open("myfile.tsv", "w+", encoding="utf8")
+    needZiZhaoImg = True
     
     def __init__(self, category=None, *args, **kwargs):
         self.loginform['TPL_username'] = raw_input("username:")
-        self.loginform['TPL_password'] = raw_input("password:")
+        self.loginform['TPL_password'] = raw_input("username:")
         print self.loginform
     
     def start_requests(self):
@@ -70,7 +72,7 @@ class TmallBrandsSpider(scrapy.Spider):
             localPic.close() 
             print "please go to %s, open file"%path  
             checkCode = raw_input("input code:")
-            print checkCode, type(checkCode)
+            #print checkCode, type(checkCode)
             return checkCode
         else:
             print "failed to get Check Code, status:", response.status
@@ -101,31 +103,82 @@ class TmallBrandsSpider(scrapy.Spider):
     
     def parse_brand(self, response):
         print "=="*10+" step 7 " + "="*10 + "start"
+        tmallBrand = TmallBrand()
+        
+        brandName = response.xpath('//ul[@class="brandWiki-con"]/li[1]/em/text()').extract()[0]
+        tmallBrand['brandName'] = brandName
         shopListLi = response.xpath('//ul[@class="brandShop-slide-list clearfix"]/li')
         for shopLi in shopListLi:
             shopName = shopLi.xpath('.//h3/text()').extract()[0]
             print shopName
             shopUrl = shopLi.xpath('./div/a/@href').extract()[0]
-            yield scrapy.Request(shopUrl, callback=self.parse_shop)
+            yield scrapy.Request(shopUrl, callback=self.parse_shop, meta=dict(tmallBrand=tmallBrand))
     
     def parse_shop(self, response):
         print "=="*10+" step 8 " + "="*10 + "start"
+        tmallBrand = response.meta['tmallBrand']
         print response.encoding
         shopInfo = response.xpath('//div[@id="shop-info"]')
         shopName = response.xpath('//a[@class="slogo-shopname"]/strong/text()').extract()[0]
+        tmallBrand['shopName'] = shopName
         shopTemp = shopInfo.xpath('.//div[contains(@class, "extend")]/ul')
-        shopCompany = shopTemp.xpath('./li[3]/div/text()').extract()[0].strip()
-        shopLocation = shopTemp.xpath('./li[4]/div/text()').extract()[0].strip()
-        wangwangUrl = "http://amos.alicdn.com/getcid.aw?spm=a1z10.1-b.1997427721.6.TW1mHd&v=3&site=cntaobao&groupid=0&s=1&uid=%s"%shopName
-        ziZhaoUrlNode = shopTemp.xpath('./li[5]/div/a/@href')
+        #tmallBrand['shopBrand'] = response.xpath('//a[contains(@class, "shop-logo")]/span/text()').extract()[0].strip()
+        tmallBrand['companyName'] = shopTemp.xpath('./li[3]/div/text()').extract()[0].strip()
+        tmallBrand['shopLocation'] = shopTemp.xpath('./li[4]/div/text()').extract()[0].strip()
+        tmallBrand['wangwangUrl'] = "http://amos.alicdn.com/getcid.aw?spm=a1z10.1-b.1997427721.6.TW1mHd&v=3&site=cntaobao&groupid=0&s=1&uid=%s"%shopName
+        ziZhaoUrlNode = response.xpath('//a[@class="tm-gsLink"]/@href')
         ziZhaoUrl = ""
         if len(ziZhaoUrlNode) > 0:
             ziZhaoUrl = ziZhaoUrlNode.extract()[0].strip()
-        string = shopName + "\t" + shopCompany + "\t" + shopLocation + "\t" + ziZhaoUrl + "\t" + wangwangUrl + "\n"
-        print string
-        self.f.write(string)
-        self.f.flush()
-        print "end write"
+        tmallBrand['ziZhaoUrl'] = ziZhaoUrl
         
+        shopType = ""
+        if shopName.find("旗舰".decode("utf8")) > -1:
+            shopType = "旗舰店"
+        elif shopName.find("专卖".decode("utf8")) > -1:
+            shopType = "专卖店"
+        else:
+            shopType = "其它"
+        tmallBrand['shopType'] = shopType
+        
+        if self.needZiZhaoImg == True and len(ziZhaoUrl) > 0:
+            xid = re.search("xid=(.*)$", ziZhaoUrl).group(1)
+            yield scrapy.Request(ziZhaoUrl, callback=self.fetch_ziZhaoImg, meta=dict(xid=xid, shopName=shopName))
+        
+        print tmallBrand
+        #self.f.write(string)
+        #self.f.flush()
+        print "end write"
+    
+    def fetch_ziZhaoImg(self, response):
+        print "=="*10+" step 9 " + "="*10 + "start"
+        shopName = response.meta['shopName']
+        xid = response.meta['xid']
+        tbtoken = response.xpath('//input[@name="_tb_token_"]/@value').extract()[0]
+        checkCode = self.getCheckCode("http://pin.aliyun.com/get_img?sessionid=ALI4e00ec7fa164bfa16c5762b9d7330ddf&identity=zhaoshang_sellermanager")
+        url = 'http://zhaoshang.mall.taobao.com/maintaininfo/liangzhao.htm?_tb_token_=%(tbtoken)s&checkCode=%(checkCode)s&xid=%(xid)s'%{'xid': xid, 'tbtoken': tbtoken, 'checkCode': checkCode}
+        print url
+        yield scrapy.Request(url, callback=self.save_ziZhaoImg, meta=dict(shopName=shopName))
+    
+    def save_ziZhaoImg(self, response):
+        print "=="*10+" step 10 " + "="*10 + "start"
+        shopName = response.meta['shopName']
+        imgNode = response.xpath('//div[@class="box-item img-box"]//img/@src').extract()
+        if len(imgNode) > 0:
+            imgUrl = imgNode[0]
+        else:
+            print "===="*20
+            print response.body
+            print "===="*20
+            print "failed to get Image"
+            return
+        print "imgUrl:%s"%imgUrl
+        req = urllib2.Request(imgUrl)
+        res = urllib2.urlopen(req)
+        fileName = "/Users/owen/Tmall/" + shopName + '.png'
+        f = open(fileName, 'wb+')
+        f.write(res.read())
+        f.close()
+        print "saved file:" + fileName
         
         
